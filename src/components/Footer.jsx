04fd78c18a2447befc001b85081212c1a05c123b
Oleg Demakov <oleg.demakov@epfl.ch>
2023-05-08 16:42:44 +0200

    CEDE footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,12 +43,38 @@ class SiteFooter extends React.Component {
     const showLanguageSelector = supportedLanguages.length > 0 && onLanguageSelected;
     const { config } = this.context;
 
+    if (config.CATALOG_BASE_URL) {
+      var tos_url = `${config.CATALOG_BASE_URL}general-terms-and-conditions/`
+      var privacy_url = `${config.CATALOG_BASE_URL}privacy-policy/`
+    } else {
+      var tos_url = `https://swissmooc.ch/general-terms-and-conditions/`
+      var privacy_url = `https://swissmooc.ch/privacy-policy/`
+    }
+
     return (
       <footer
         role="contentinfo"
         className="footer d-flex border-top py-3 px-4"
       >
         <div className="container-fluid d-flex">
+          <ul className="list-unstyled">
+            <li>
+              {showLanguageSelector && (
+                  <LanguageSelector
+                    options={supportedLanguages}
+                    onSubmit={onLanguageSelected}
+                  />
+              )}
+            </li>
+            <li style={{ fontSize: "0.6875em" }} className="pt-1">
+              © {config.SITE_NAME}. All rights reserved except where noted.  edX, Open edX and their respective logos are registered trademarks of edX Inc.
+            </li>
+            <li style={{ fontSize: "0.6875em" }} className="pt-1">
+              <a className="text-primary-500 mr-1" href={tos_url}>{intl.formatMessage(messages['footer.catalogLinks.tos'])}</a>
+              <a className="text-primary-500 ml-1" href={privacy_url}>{intl.formatMessage(messages['footer.catalogLinks.privacy'])}</a>
+            </li>
+          </ul>
+          <div className="flex-grow-1" />
           <a
             className="d-block"
             href={config.LMS_BASE_URL}
@@ -60,13 +86,6 @@ class SiteFooter extends React.Component {
               alt={intl.formatMessage(messages['footer.logo.altText'])}
             />
           </a>
-          <div className="flex-grow-1" />
-          {showLanguageSelector && (
-            <LanguageSelector
-              options={supportedLanguages}
-              onSubmit={onLanguageSelected}
-            />
-          )}
         </div>
       </footer>
     );
