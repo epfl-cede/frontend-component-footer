5e3d2f895e0921423f5777112ce7b0377e1b43a4
Olivier Scherler <olivier.scherler@epfl.ch>
2025-09-18 11:42:52 +0200

    Replace language selector label with icon.

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, intlShape, FormattedMessage } from '@edx/frontend-platform/i18n';
+import { Icon } from '@openedx/paragon';
+import { Language } from '@openedx/paragon/icons';
 
 const LanguageSelector = ({
   intl, options, onSubmit, ...props
@@ -19,11 +21,14 @@ const LanguageSelector = ({
       <div className="form-group">
         {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
         <label htmlFor="site-footer-language-select" className="d-inline-block m-0">
-          <FormattedMessage
-            id="footer.languageForm.select.label"
-            defaultMessage="Choose Language"
-            description="The label for the laguage select part of the language selection form."
-          />
+          <Icon src={Language}/>
+          <span className="sr-only">
+            <FormattedMessage
+              id="footer.languageForm.select.label"
+              defaultMessage="Choose Language"
+              description="The label for the laguage select part of the language selection form."
+            />
+          </span>
         </label>
         <select
           id="site-footer-language-select"
