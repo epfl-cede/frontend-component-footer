a18dc30e786737eee54da3eb87935a6d77cc230f
Oleg Demakov <oleg.demakov@epfl.ch>
2025-10-28 14:01:30 +0100

    Ignore dist folder

--- dist/components/LanguageSelector.js
+++ dist/components/LanguageSelector.js
@@ -1,10 +1,12 @@
 var _excluded = ["intl", "options", "onSubmit"];
-function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+function _extends() { return _extends = Object.assign ? Object.assign.bind() : function (n) { for (var e = 1; e < arguments.length; e++) { var t = arguments[e]; for (var r in t) ({}).hasOwnProperty.call(t, r) && (n[r] = t[r]); } return n; }, _extends.apply(null, arguments); }
+function _objectWithoutProperties(e, t) { if (null == e) return {}; var o, r, i = _objectWithoutPropertiesLoose(e, t); if (Object.getOwnPropertySymbols) { var n = Object.getOwnPropertySymbols(e); for (r = 0; r < n.length; r++) o = n[r], -1 === t.indexOf(o) && {}.propertyIsEnumerable.call(e, o) && (i[o] = e[o]); } return i; }
+function _objectWithoutPropertiesLoose(r, e) { if (null == r) return {}; var t = {}; for (var n in r) if ({}.hasOwnProperty.call(r, n)) { if (-1 !== e.indexOf(n)) continue; t[n] = r[n]; } return t; }
 import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, intlShape, FormattedMessage } from '@edx/frontend-platform/i18n';
+import { Icon } from '@openedx/paragon';
+import { Language } from '@openedx/paragon/icons';
 var LanguageSelector = function LanguageSelector(_ref) {
   var intl = _ref.intl,
     options = _ref.options,
@@ -12,26 +14,33 @@ var LanguageSelector = function LanguageSelector(_ref) {
     props = _objectWithoutProperties(_ref, _excluded);
   var handleSubmit = function handleSubmit(e) {
     e.preventDefault();
-    var languageCode = e.target.elements['site-footer-language-select'].value;
+    var languageCode = e.target.value;
     onSubmit(languageCode);
   };
+  options.sort(function (a, b) {
+    return a.label.localeCompare(b.label);
+  });
   return /*#__PURE__*/React.createElement("form", _extends({
-    className: "form-inline",
-    onSubmit: handleSubmit
+    className: "form-inline"
   }, props), /*#__PURE__*/React.createElement("div", {
     className: "form-group"
   }, /*#__PURE__*/React.createElement("label", {
     htmlFor: "site-footer-language-select",
     className: "d-inline-block m-0"
+  }, /*#__PURE__*/React.createElement(Icon, {
+    src: Language
+  }), /*#__PURE__*/React.createElement("span", {
+    className: "sr-only"
   }, /*#__PURE__*/React.createElement(FormattedMessage, {
     id: "footer.languageForm.select.label",
     defaultMessage: "Choose Language",
     description: "The label for the laguage select part of the language selection form."
-  })), /*#__PURE__*/React.createElement("select", {
+  }))), /*#__PURE__*/React.createElement("select", {
     id: "site-footer-language-select",
     className: "form-control-sm mx-2",
     name: "site-footer-language-select",
-    defaultValue: intl.locale
+    defaultValue: intl.locale,
+    onChange: handleSubmit
   }, options.map(function (_ref2) {
     var value = _ref2.value,
       label = _ref2.label;
@@ -39,14 +48,6 @@ var LanguageSelector = function LanguageSelector(_ref) {
       key: value,
       value: value
     }, label);
-  })), /*#__PURE__*/React.createElement("button", {
-    "data-testid": "site-footer-submit-btn",
-    className: "btn btn-outline-primary btn-sm",
-    type: "submit"
-  }, /*#__PURE__*/React.createElement(FormattedMessage, {
-    id: "footer.languageForm.submit.label",
-    defaultMessage: "Apply",
-    description: "The label for button to submit the language selection form."
   }))));
 };
 LanguageSelector.propTypes = {
