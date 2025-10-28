e721399adea9279b8e6790cd0a8ded59892e43f3
Oleg Demakov <oleg.demakov@epfl.ch>
2025-10-28 14:01:30 +0100

    Ignore dist folder

--- dist/components/Footer.js
+++ dist/components/Footer.js
@@ -1,16 +1,16 @@
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _callSuper(t, o, e) { return o = _getPrototypeOf(o), _possibleConstructorReturn(t, _isNativeReflectConstruct() ? Reflect.construct(o, e || [], _getPrototypeOf(t).constructor) : o.apply(t, e)); }
+function _possibleConstructorReturn(t, e) { if (e && ("object" == _typeof(e) || "function" == typeof e)) return e; if (void 0 !== e) throw new TypeError("Derived constructors may only return object or undefined"); return _assertThisInitialized(t); }
+function _assertThisInitialized(e) { if (void 0 === e) throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); return e; }
 function _isNativeReflectConstruct() { try { var t = !Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); } catch (t) {} return (_isNativeReflectConstruct = function _isNativeReflectConstruct() { return !!t; })(); }
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _getPrototypeOf(t) { return _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function (t) { return t.__proto__ || Object.getPrototypeOf(t); }, _getPrototypeOf(t); }
+function _inherits(t, e) { if ("function" != typeof e && null !== e) throw new TypeError("Super expression must either be null or a function"); t.prototype = Object.create(e && e.prototype, { constructor: { value: t, writable: !0, configurable: !0 } }), Object.defineProperty(t, "prototype", { writable: !1 }), e && _setPrototypeOf(t, e); }
+function _setPrototypeOf(t, e) { return _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function (t, e) { return t.__proto__ = e, t; }, _setPrototypeOf(t, e); }
 import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
@@ -24,16 +24,15 @@ var EVENT_NAMES = {
   FOOTER_LINK: 'edx.bi.footer.link'
 };
 var SiteFooter = /*#__PURE__*/function (_React$Component) {
-  _inherits(SiteFooter, _React$Component);
-  var _super = _createSuper(SiteFooter);
   function SiteFooter(props) {
     var _this;
     _classCallCheck(this, SiteFooter);
-    _this = _super.call(this, props);
-    _this.externalLinkClickHandler = _this.externalLinkClickHandler.bind(_assertThisInitialized(_this));
+    _this = _callSuper(this, SiteFooter, [props]);
+    _this.externalLinkClickHandler = _this.externalLinkClickHandler.bind(_this);
     return _this;
   }
-  _createClass(SiteFooter, [{
+  _inherits(SiteFooter, _React$Component);
+  return _createClass(SiteFooter, [{
     key: "externalLinkClickHandler",
     value: function externalLinkClickHandler(event) {
       var label = event.currentTarget.getAttribute('href');
@@ -52,8 +51,10 @@ var SiteFooter = /*#__PURE__*/function (_React$Component) {
         onLanguageSelected = _this$props.onLanguageSelected,
         logo = _this$props.logo,
         intl = _this$props.intl;
-      var showLanguageSelector = supportedLanguages.length > 0 && onLanguageSelected;
       var config = getConfig();
+      var configLanguages = config.INDIGO_SUPPORTED_LANGUAGES;
+      var languages = configLanguages !== null && configLanguages !== void 0 ? configLanguages : supportedLanguages;
+      var showLanguageSelector = languages.length > 0 && onLanguageSelected;
       return /*#__PURE__*/React.createElement("div", {
         className: "wrapper wrapper-footer"
       }, /*#__PURE__*/React.createElement("footer", {
@@ -92,12 +93,11 @@ var SiteFooter = /*#__PURE__*/function (_React$Component) {
       })))))), /*#__PURE__*/React.createElement("span", {
         className: "copyright-site"
       }, intl.formatMessage(messages['footer.copyright.text'])), showLanguageSelector && /*#__PURE__*/React.createElement(LanguageSelector, {
-        options: supportedLanguages,
+        options: languages,
         onSubmit: onLanguageSelected
       })));
     }
   }]);
-  return SiteFooter;
 }(React.Component);
 SiteFooter.contextType = AppContext;
 SiteFooter.propTypes = {
